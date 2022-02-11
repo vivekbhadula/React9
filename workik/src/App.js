b0485c60e9681c106cdbf6e3fdd3b49bc4792783
Vivek Bhadula <vivekbhadula201420@gmail.com>
2022-02-11 11:43:38 +0530

    implementing antD

--- workik/src/App.js
+++ workik/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import FormInput from './components/FormInput';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <FormInput />
     </div>
   );
 }
